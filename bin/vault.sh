de113d6296cb9b91b53615ce766829950f292225
archangel189 <archanshell@gmail.com>
2019-06-18 00:03:49 +1000

    Refactor vault_login()

--- bin/vault.sh
+++ bin/vault.sh
@@ -1,12 +1,11 @@
 vault_login() {
     export VAULT_ADDR=https://vault.example.com
+    export VAULT_CLIENT_TIMEOUT=3
+    export VAULT_MAX_RETRIES=0
     export http_proxy=http://vault.example.com:8888
     export https_proxy=http://vault.example.com:8888
     export HTTP_PROXY=http://vault.example.com:8888
     export HTTPS_PROXY=http://vault.example.com:8888
-    export no_proxy='*.local, localhost, 127.0.0.1, 169.254/16'
-    export VAULT_CLIENT_TIMEOUT=3
-    export VAULT_MAX_RETRIES=0
 
     # For users in the the AU domain
     vault login -method=ldap username=$1
