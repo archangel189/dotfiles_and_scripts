757fa53c302c606bb5169c158c7b63e2180d970d
Arthur Rimbun <arthur.rimbun@cba.com.au>
2019-07-17 22:44:44 +1000

    Update dev setup to install dotfiles

--- bin/dev-setup.sh
+++ bin/dev-setup.sh
@@ -6,6 +6,9 @@
 # Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install dotfiles
+cp .* $HOME
+
 # Homebrew packages
 brew install go gomplate kubectl
 
